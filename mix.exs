230856388be35f17d8d603f1e4fae669bf6796b4
Levi Aul <levi@leviaul.com>
2018-06-19 20:38:01 -0700

    Add Hex required fields

--- mix.exs
+++ mix.exs
@@ -6,14 +6,36 @@ defmodule ETFs.MixProject do
       app: :etfs,
       version: "0.1.0",
       elixir: "~> 1.6",
+      description: description(),
+      package: package(),
       start_permanent: Mix.env() == :prod,
-      deps: []
+      deps: deps()
     ]
   end
+
+  defp description do
+    """
+    A simple streamable binary-serialization container format based on Erlang's External Term Format
+    """
+  end
+
+  defp package do [
+    name: :etfs,
+    files: ["config", "lib", "mix.exs", "LICENSE"],
+    maintainers: ["Levi Aul"],
+    licenses: ["MIT"],
+    links: %{"GitHub" => "https://github.com/tsutsu/etfs"}
+  ] end
 
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  def deps do
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 end
